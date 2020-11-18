1e6d42f386c7a604abfc9f4368ee77bcb013ba96
Nana Lau <nanalau315@hotmail.com>
2020-11-18 15:59:15 -0500

    working process

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+    attr_accessor :name, :grade, :roster
+    def initialize(name)
+        @name = name
+    end
+    def roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+        @roster.each do |g, n|
+            @roster[@grade] = []
+            @roster[@grade] << @name
+        end
+    end
+end
